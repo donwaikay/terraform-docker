277e33247aa05d495b67eab881dfc4596e3d803e
donwaikay <donwaikay@gmail.com>
2022-06-13 00:34:31 -0400

    main.tf added

---
+++ main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.16.0"
+    }
+  }
+}
+
+
+provider "docker" {}
+
+resource "docker_image" "nodered_image" {
+  name = "nodered/node-red:latest"
+}
+
+resource "docker_container" "nodered_container" {
+  count = 2
+  name  = join("-",["nodered", random_string.random[count.index].result])
+  image = docker_image.nodered_image.latest
+  ports {
+    internal = 1880
+  # external = 1880
+  }
+}
+
+resource "random_string" "random" {
+  count = 2
+  length = 4
+  special = false
+  upper = false
+}
+
+
+output "container-name" {
+  value = docker_container.nodered_container[*].name
+  description = "The name of the container"
+}
+
+output "ip-address2" {
+  value = [for i in docker_container.nodered_container[*] : join(":", [i.ip_address], i.ports[*]["external"])]
+  description = "The ip address and external port of the container"
+}
